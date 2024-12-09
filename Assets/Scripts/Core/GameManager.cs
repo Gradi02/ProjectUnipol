80b8002cd8107c3ac184f94bba82c84b9b7f26c8
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-12-09 19:36:04 +0100

    gameloop upgrade

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,19 +1,28 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private PlayerOverlayCard[] uiPlayersCards;
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private Shader shader;
-    
+    [SerializeField] private GameObject transitionCanva, playerTurnCanva, cardCanva, buyCanva;
+    [SerializeField] private TextMeshProUGUI transitionText, buyText;
+    [SerializeField] private Button rollButton, surrenderButton;
+
 
     public static GameManager instance { get; private set; }
     public List<Player> players { get; private set; } = new List<Player>();
     public int currentPlayerIndex { get; private set; } = 0;
-    
-    
+
+
     public Board board;
+
+    private bool dublet = false, canStartNextTurn = true, isGameStarted = false;
 
 
 
@@ -21,6 +30,11 @@ public class GameManager : MonoBehaviour
     {
         if (instance == null) instance = this;
         else Destroy(gameObject);
+
+        transitionCanva.SetActive(false);
+        playerTurnCanva.SetActive(false);
+        cardCanva.SetActive(false);
+        buyCanva.SetActive(false);
     }
 
     public void StartGame(PlayerUICard[] p)
@@ -37,36 +51,171 @@ public class GameManager : MonoBehaviour
                 newPlayer.GetComponent<MeshRenderer>().material = mat;
 
                 uiPlayersCards[idx].Setup(p[i]);
-                newPlayerScript.SetUp(uiPlayersCards[idx]);
+                newPlayerScript.SetUp(uiPlayersCards[idx], p[i].usernameText);
                 idx++;
 
                 players.Add(newPlayerScript);
             }
         }
 
-        for(int i = idx; i < uiPlayersCards.Length; i++)
+        for (int i = idx; i < uiPlayersCards.Length; i++)
         {
             uiPlayersCards[i].Disabled();
         }
 
         board.SetupPlayers(players);
+
+        currentPlayerIndex = Random.Range(0, players.Count);
+        isGameStarted = true;
+    }
+
+    private void Update()
+    {
+        if(isGameStarted && canStartNextTurn)
+        {
+            if (dublet)
+            {
+                dublet = false;
+                canStartNextTurn = false;
+                ShowPlayerState(true);
+            }
+            else
+            {
+                NextPlayer();
+            }
+        }
     }
 
 
+    //BUTTONS
 
-    public void NextTurn()
+    public void RollButton()
     {
+        playerTurnCanva.SetActive(false);
+
         int k1 = Random.Range(1, 6);
         int k2 = Random.Range(1, 6);
         Debug.Log($"k1: {k1}, k2: {k2}");
 
         int num = k1 + k2;
+        if (k1 == k2)
+        {
+            StartCoroutine(IETransitionState("Double Throw!"));
+            dublet = true;
+        }
+
         board.MovePlayer(players[currentPlayerIndex], num);
+    }
 
-        if (k1 != k2)
-            currentPlayerIndex++;
+    public void SurrenderButton()
+    {
+        playerTurnCanva.SetActive(false);
 
+        players[currentPlayerIndex].Surrender();
+        NextPlayer();
+    }
+
+    public void BuyButtonYes()
+    {
+        Player currentPlayer = players[currentPlayerIndex];
+        BoardField field = board.fields[currentPlayer.currentPosition];
+
+        if (field.property.owner == null)
+        {
+            field.property.owner = currentPlayer;
+            currentPlayer.money -= field.property.price;
+            field.gameObject.GetComponent<MeshRenderer>().material = currentPlayer.gameObject.GetComponent<MeshRenderer>().material;
+        }
+        else
+        {
+            currentPlayer.money -= field.property.upgradePrice[field.property.level];
+            field.property.level++;
+        }
+
+        currentPlayer.AddFieldToList(field);
+        buyCanva.SetActive(false);
+        canStartNextTurn = true;
+    }
+
+    public void BuyButtonNo()
+    {
+        buyCanva.SetActive(false);
+        canStartNextTurn = true;
+    }
+
+
+
+    //GameStates
+
+    private void NextPlayer()
+    {
+        canStartNextTurn = false;
+
+        currentPlayerIndex++;
         if (currentPlayerIndex >= players.Count)
             currentPlayerIndex = 0;
+
+        while (!players[currentPlayerIndex].isActive)
+        {
+            currentPlayerIndex++;
+            if (currentPlayerIndex >= players.Count)
+                currentPlayerIndex = 0;
+        }
+        
+        string t = $"{players[currentPlayerIndex].playerName}'s Turn!";
+        StartCoroutine(IETransitionState(t, true));
+    }
+
+    private void ShowPlayerState(bool show)
+    {
+        rollButton.interactable = true;
+        surrenderButton.interactable = true;
+        playerTurnCanva.SetActive(show);
+    }
+
+
+    //after landed on new tail returns function
+    public void AskForBuyOrUpgrade(string ask)
+    {
+        buyText.text = ask;
+        buyCanva.SetActive(true);
+    }
+
+    public void NotEnoughtMoneyInfo(string ask)
+    {
+        StartCoroutine(IETransitionState(ask, false, true));
+    }
+
+    public void StandOnSpecialCard()
+    {
+        string t = $"Karta Specjalne TODO: EVENT";
+        StartCoroutine(IETransitionState(t, false, true));
+    }
+
+    public void PaidAction(Player payer, Player owner, int price, string p)
+    {
+        StartCoroutine(IETransitionState(p, false, true));
+        payer.money -= price;
+        owner.money += price;
+    }
+
+    public void ElseState()
+    {
+        canStartNextTurn = true;
+    }
+
+
+
+    private IEnumerator IETransitionState(string title, bool nextIE = false, bool nextPlayer = false)
+    {
+        transitionText.text = title;
+        transitionCanva.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        transitionCanva.SetActive(false);
+
+        if (nextIE)
+            ShowPlayerState(true);
+        else if(nextPlayer)
+            canStartNextTurn = true;
     }
 }
